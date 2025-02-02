578e50e413c5fa215948112939805090a9eeeaa2
Rey Omar Esguerra Ramirez <reyomar066@gmail.com>
2025-02-01 22:32:17 -0500

    ProductsViewComponent, HelperFunctionToGetAllCategories, ProductGridComponent and Implementing Framer Motion

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,20 +1,17 @@
-import { Button } from "@/components/ui/button";
+import ProductsView from "@/components/ProductsView";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
 export default async function Home() {
   const products = await getAllProducts();
-
-  // console.log(
-  //  crypto.randomUUID().slice(0, 5) +
-  //      `>>> Rendered the home page cache with ${products.length} products and ${categories.length} categories`
-  // );
+  const categories = await getAllCategories();
 
   return (
     <div>
       <h1>Hello</h1>
       
-      <div>
-        <ProductsView products={products}/>
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductsView products={products} categories={categories}/>
       </div>
     </div>
   );
