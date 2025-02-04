718df8e983b24c4212f60e05c17fcc11d03e5e2e
Rey Omar Esguerra Ramirez <reyomar066@gmail.com>
2025-02-04 18:14:46 -0500

    Search Page - Working

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,3 +1,4 @@
+import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
 
 async function SearchPage({
     searchParams,
@@ -7,6 +8,22 @@ async function SearchPage({
     };
 }) {
     const { query } = await searchParams;
+    const products = await searchProductsByName(query);
+
+    if (!products.length) {
+        return (
+            <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+                <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+                    <h1 className="text-3xl font-bold mb-6 text-center">
+                        No products for: {query}
+                    </h1>
+                    <p className="text-gray-600 text-center">
+                        Try searching for something else
+                    </p>
+                </div>
+            </div>
+        );
+    }
 
     return <div>SearchPage for {query}</div>
 }
